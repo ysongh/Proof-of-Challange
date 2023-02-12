ee58f2179a96b171807f7442414f54638b6ae7bf
You Song Hou <yousonghou@gmail.com>
2023-02-11 21:20:06 -0500

    feat: integrate Arcana Auth

---
+++ src/lib/auth.js
@@ -0,0 +1,17 @@
+import { AuthProvider } from '@arcana/auth'
+import { VUE_APP_ARCANA_APP_ADDRESS } from '../keys'
+let authInstance
+
+export async function getAuthInstance () {
+  if (authInstance == null) {
+    authInstance = new AuthProvider(VUE_APP_ARCANA_APP_ADDRESS, {
+      network: process.env.VUE_APP_ARCANA_NETWORK ?? 'dev',
+      alwaysVisible: true,
+      theme: 'light',
+      position: 'right'
+    })
+    await authInstance.init()
+    await authInstance.connect()
+  }
+  return authInstance
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { getAuthInstance } from '@/lib/auth'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  async mounted () {
+    // eslint-disable-next-line no-unused-vars
+    const a = await getAuthInstance()
+    // Provider available at a.provider
   }
 }
 </script>
