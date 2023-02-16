1898169adb6988f8766781015b60c7fda0e9c841
You Song Hou <ysongweb3@gmail.com>
2023-02-16 00:33:49 -0500

    feat: update hardhat.config.js to deploy a contract on Mantle Testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    // npx hardhat run scripts/deploy.js --network mantle-testnet
+    "mantle-testnet": {
+      url: "https://rpc.testnet.mantle.xyz/",
+      accounts: [process.env.PRIVATEKEY],
+      chainId: 5001,
+      gasPrice: 8000000000
+    }
+  }
 };
